4ab667d30d9f7b20fecfe49b5bd803ae19856f37
Gabrielle Lattanzi <82163272+glattanz@users.noreply.github.com>
2022-02-17 18:49:45 -0300

    Update 11-02-22.cpp

--- 11-02-22.cpp
+++ 11-02-22.cpp
@@ -5,8 +5,8 @@
 
 float funcao(float x){
 
-//return (x*log(x))-3.2;
-return ((x*x)-9);
+return (x*log(x))-3.2;
+//return ((x*x)-9);
 		
 }
 
@@ -14,7 +14,7 @@ int main(){
 	
 	setlocale( LC_ALL, "" );
 	
-float a,b,c,e, med, x, w;
+float a,b,c,e=0, med, x, w;
 float abs,cont=0,fa,fb, fx, fy=0;
 	
 	printf ("Informe o valor do limite inferior (A):");
@@ -46,13 +46,16 @@ float abs,cont=0,fa,fb, fx, fy=0;
 	}
 	
 	while(cont<100){
-		printf ("\nInforme o número máximo de interações:\n");
+		printf ("\nInforme o número máximo de interações:");
 		scanf ("%f",&cont);
 		
 		if(cont<100){
 			printf("\nInsira um valor maior do que 100\n");
 		}
 	}
+	
+	printf ("\nInforme o valor do erro:");
+	scanf ("%f",&e);
 	
 	while(e>0.1){
 		printf ("\nInforme o valor do erro:");
@@ -61,34 +64,38 @@ float abs,cont=0,fa,fb, fx, fy=0;
 	
 	x=(a+b)/2;
 	
-	printf("Média: %f", x);
-	
 	fx=funcao(x);
 	
 	int i=1;
 	
-	printf("====================================================");
-	printf("\nK\tA\tMédia\tB\tf(a)\tf(med)  f(b)\tf(y)");
-	printf("\n%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f", i, a, x, b, fa, fx, fb, fy);
+	printf("\n====================================================");
+	printf("\nK\tA\tMédia\tB\tf(a)\tf(med)  f(b)");
+	printf("\n%d\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f", i, a, x, b, fa, fx, fb);
 	
-	for (i=1; i<cont||fabs(fx)<e ; i++){
+	for (i=2; i<cont&&fabs(fx)>e ; i++){
 		fy=(fa*fx);
 		if(fy<0){
 			b=x;
 			fa=funcao(a);
 			fb=funcao(b);
 			
-			//printf("%f\n", fa*fx);
-			printf("\n%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f", i, a, x, b, fa, fx, fb, fy);
+			printf("\n%d\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f", i, a, x, b, fa, fx, fb);
+			
+			
 		}
 		if(fy>=0){
 			a=x;
 			fa=funcao(a);
 			fb=funcao(b);
+		
+		printf("\n%d\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f", i, a, x, b, fa, fx, fb);
 			
-			//printf("%f\n", fa*fx);
-			printf("\n%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f", i, a, x, b, fa, fx, fb, fy);
 		}
+		
+		//Recalcular a função
+		x=(a+b)/2;
+	
+		fx=funcao(x);
 	}
 	
 
